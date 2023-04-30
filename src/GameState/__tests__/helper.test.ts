c1f89edde0ff9b1810e2d7dd41e468cfc2c9076f
da593 <dylanau99@gmail.com>
2023-04-30 17:01:47 -0400

    Add unit tests for helper calculations

--- src/GameState/__tests__/helper.test.ts
+++ src/GameState/__tests__/helper.test.ts
@@ -1,5 +1,6 @@
 import { GAME_DIMENSIONS } from "GameState/constants";
-import { calculateBirdCoords, calculateGapCoords, calculateNewBirdCoords, calculateNewGapCoords, detectCollision, randomizeYGapLocation } from "GameState/helper";
+import { calculateBirdCoords, calculateGapCoords, calculateNewBirdCoords, calculateNewGapCoords, detectCollision } from "GameState/helper";
+import* as randomizeGapFunc from "GameState/randomizeGapFunc";
 import { BoxCoordinates } from "GameState/types";
 
 const birdAtCeiling:BoxCoordinates = {
@@ -37,49 +38,66 @@ const gapOffScreen:BoxCoordinates = {
     botRight:{x: 0, y: -5 + GAME_DIMENSIONS.GAP_HEIGHT}
 };
 
-test("no collision when bird hits top of box", () => {
-    expect(detectCollision(birdAtCeiling, gapAtRight)).toBe(false);
-});
 
-test("no collision when bird is in gap", () => {
-    expect(detectCollision(birdAtCeiling, gapAtBird)).toBe(false);
-});
-
-test("collision when bird hits pipe", () => {
-    expect(detectCollision(birdAtCeiling, pipeAtBird)).toBe(true);
-});
-
-test("calculate bird coordinates given bird x location", () => {
-    expect(calculateBirdCoords(0)).toEqual(birdAtCeiling);
-});
-
-test("calculate gap coordinates given x and y location", () => {
-    expect(calculateGapCoords(GAME_DIMENSIONS.BIRD_X_LOCATION, -5)).toEqual(gapAtBird);
-});
-
-
-test("calculate new bird coordinates given previous coordinates", () => {
-    expect(calculateNewBirdCoords(birdAtCeiling)).toEqual({
-        topLeft:{x: GAME_DIMENSIONS.BIRD_X_LOCATION, y: 1}, 
-        topRight:{x: GAME_DIMENSIONS.BIRD_X_LOCATION + GAME_DIMENSIONS.BIRD_WIDTH, y: 1}, 
-        botLeft:{x: GAME_DIMENSIONS.BIRD_X_LOCATION, y: GAME_DIMENSIONS.BIRD_WIDTH + 1}, 
-        botRight:{x: GAME_DIMENSIONS.BIRD_X_LOCATION + GAME_DIMENSIONS.BIRD_WIDTH, y:  GAME_DIMENSIONS.BIRD_WIDTH + 1}
+describe("test detect collision function", () => {
+    test("no collision when bird hits top of box", () => {
+        expect(detectCollision(birdAtCeiling, gapAtRight)).toBe(false);
     });
-});
+    
+    test("no collision when bird is in gap", () => {
+        expect(detectCollision(birdAtCeiling, gapAtBird)).toBe(false);
+    });
+    
+    test("collision when bird hits pipe", () => {
+        expect(detectCollision(birdAtCeiling, pipeAtBird)).toBe(true);
+    });
+})
 
-test("calculate new gap coordinates given previous coordinates", () => {
-    expect(calculateNewGapCoords(gapAtBird)).toEqual({
-        topLeft:{x: GAME_DIMENSIONS.BIRD_X_LOCATION - 1, y: -5}, 
-        topRight:{x: GAME_DIMENSIONS.BIRD_X_LOCATION + GAME_DIMENSIONS.PIPE_WIDTH  - 1, y: -5}, 
-        botLeft:{x:  GAME_DIMENSIONS.BIRD_X_LOCATION - 1, y:  -5 + GAME_DIMENSIONS.GAP_HEIGHT}, 
-        botRight:{x: GAME_DIMENSIONS.BIRD_X_LOCATION + GAME_DIMENSIONS.PIPE_WIDTH - 1, y: -5 + GAME_DIMENSIONS.GAP_HEIGHT}
+describe("test gap coordinates calculations", () => {
+    test("calculate gap coordinates given x and y location", () => {
+        expect(calculateGapCoords(GAME_DIMENSIONS.BIRD_X_LOCATION, -5)).toEqual(gapAtBird);
     });
 
-    expect(calculateNewGapCoords(gapOffScreen)).toMatchObject({
-        topLeft:{x: GAME_DIMENSIONS.GAME_WIDTH},
-        topRight:{x: GAME_DIMENSIONS.GAME_WIDTH + GAME_DIMENSIONS.PIPE_WIDTH}, 
-        botLeft:{x:  GAME_DIMENSIONS.GAME_WIDTH}, 
-        botRight:{x: GAME_DIMENSIONS.GAME_WIDTH + GAME_DIMENSIONS.PIPE_WIDTH}
+    test("calculate new gap coordinates given previous coordinates", () => {
+        expect(calculateNewGapCoords(gapAtBird)).toEqual({
+            topLeft:{x: GAME_DIMENSIONS.BIRD_X_LOCATION - 1, y: -5}, 
+            topRight:{x: GAME_DIMENSIONS.BIRD_X_LOCATION + GAME_DIMENSIONS.PIPE_WIDTH  - 1, y: -5}, 
+            botLeft:{x:  GAME_DIMENSIONS.BIRD_X_LOCATION - 1, y:  -5 + GAME_DIMENSIONS.GAP_HEIGHT}, 
+            botRight:{x: GAME_DIMENSIONS.BIRD_X_LOCATION + GAME_DIMENSIONS.PIPE_WIDTH - 1, y: -5 + GAME_DIMENSIONS.GAP_HEIGHT}
+        });
+
+        expect(calculateNewGapCoords(gapOffScreen)).toMatchObject({
+            topLeft:{x: GAME_DIMENSIONS.GAME_WIDTH},
+            topRight:{x: GAME_DIMENSIONS.GAME_WIDTH + GAME_DIMENSIONS.PIPE_WIDTH}, 
+            botLeft:{x:  GAME_DIMENSIONS.GAME_WIDTH}, 
+            botRight:{x: GAME_DIMENSIONS.GAME_WIDTH + GAME_DIMENSIONS.PIPE_WIDTH}
+        });
+
+        const spy = jest.spyOn(randomizeGapFunc, 'randomizeYGapLocation');
+        spy.mockReturnValue(350);
+        const mockYGapLocation = randomizeGapFunc.randomizeYGapLocation();
+        expect(mockYGapLocation).toEqual(350);
+
+        spy.mockRestore();
     });
-});
+})
+
+describe("test bird coordinates calculations", () => {
+    test("calculate bird coordinates given bird x location", () => {
+        expect(calculateBirdCoords(0)).toEqual(birdAtCeiling);
+    });
+    
+    test("calculate new bird coordinates given previous coordinates", () => {
+        expect(calculateNewBirdCoords(birdAtCeiling)).toEqual({
+            topLeft:{x: GAME_DIMENSIONS.BIRD_X_LOCATION, y: 1}, 
+            topRight:{x: GAME_DIMENSIONS.BIRD_X_LOCATION + GAME_DIMENSIONS.BIRD_WIDTH, y: 1}, 
+            botLeft:{x: GAME_DIMENSIONS.BIRD_X_LOCATION, y: GAME_DIMENSIONS.BIRD_WIDTH + 1}, 
+            botRight:{x: GAME_DIMENSIONS.BIRD_X_LOCATION + GAME_DIMENSIONS.BIRD_WIDTH, y:  GAME_DIMENSIONS.BIRD_WIDTH + 1}
+        });
+    });
+})
+
+
+
+
 
