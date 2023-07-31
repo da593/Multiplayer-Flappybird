a9053510905eb2e36019645745d7eefd387160a8
da593 <dylanau99@gmail.com>
2023-07-30 20:20:49 -0400

    Init a next app

--- client/src/hooks/DimensionsContext.ts
+++
@@ -1,6 +0,0 @@
-import { Dimensions_I } from '@flappyblock/shared';
-import { createContext } from 'react';
-
-
-
-export const DimensionContext = createContext<Dimensions_I | undefined>(undefined);
--- client/src/hooks/socketContext.ts
+++
@@ -1,7 +0,0 @@
-import { ServerToClientEvents, ClientToServerEvents } from "@flappyblock/shared";
-import { baseUrl } from "api/axios";
-import React from "react";
-import io, { Socket } from "socket.io-client";
-
-export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(baseUrl);
-export const SocketContext = React.createContext(socket);
