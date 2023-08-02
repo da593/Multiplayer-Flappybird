8cc069f978cc7a3bacd30cd3ab3e5b65a336bb87
da593 <dylanau99@gmail.com>
2023-08-01 21:09:03 -0400

    Add npm workspace as a path alias in webpack config

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+
+const nextConfig = {
+    // experimental: {
+    //     externalDir: true,
+    // },
+    webpack(config) {
+        config.resolve.alias = {
+          ...config.resolve.alias,
+          "@flappyblock/shared" : path.resolve(__dirname, "../shared")
+        }
+        return config
+    }
+}
 
 module.exports = nextConfig
