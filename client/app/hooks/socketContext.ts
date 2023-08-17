64a0dc1cdd1934e2b81ced3f635ee5fb29eac9be
da593 <dylanau99@gmail.com>
2023-08-16 22:31:13 -0400

    Throw error if env var is undefined

--- client/app/hooks/socketContext.ts
+++ client/app/hooks/socketContext.ts
@@ -4,6 +4,14 @@ import { ServerToClientEvents, ClientToServerEvents } from "@flappyblock/shared"
 import {createContext} from "react";
 import io, { Socket } from "socket.io-client";
 
-const baseUrl: string = process.env.NEXT_PUBLIC_PORT_API;
+function getApi(): string {
+    const res = process.env.NEXT_PUBLIC_PORT_API;
+    if (res === undefined) {
+        throw new Error("Cannot find API");
+    }
+    return res;
+}
+
+const baseUrl: string = getApi();
 const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(baseUrl);
 export const SocketContext = createContext(socket);
