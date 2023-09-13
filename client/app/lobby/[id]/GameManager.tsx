9e6b8f71953108961b06186afca4c35451548d2c
da593 <dylanau99@gmail.com>
2023-09-12 21:11:34 -0400

    Add interpolation function with requestAnimationFrame to smoothen movement between server updates

--- client/app/lobby/[id]/GameManager.tsx
+++ client/app/lobby/[id]/GameManager.tsx
@@ -1,6 +1,6 @@
-import  {useState,useEffect, useContext} from "react";
+import  {useState,useEffect, useContext, useRef} from "react";
 import Link from 'next/link';
-import { BoxCoordinates, Coordinates, Events, GAME_DIMENSIONS, GameStateResponse, INITIAL_STATE, KEYBINDS, PipeState_I, PlayerState_I, ReadyCheck, WinState, calculateNewGapCoords } from "@flappyblock/shared";
+import { BoxCoordinates, Events, GAME_DIMENSIONS, GameStateResponse, INITIAL_STATE, KEYBINDS, PipeState_I, PlayerState_I, ReadyCheck, WinState, calculateNewGapCoords, game_tick } from "@flappyblock/shared";
 import { SocketContext } from "hooks/socketContext";
 import { BoardGame } from "components/BoardGame";
 import { NavigationMenu } from "components/NavigationMenu";
@@ -15,11 +15,15 @@ export function GameManager({lobbyId, playerId_self, players}:Props) {
     const socket = useContext(SocketContext);
     const [pipeState, setPipeState] = useState<PipeState_I>(INITIAL_STATE.pipe);
     const [playerStates, setPlayerStates] = useState<Record<string, PlayerState_I>>(initPlayerStates(players));
+    const [snapshot, setSnapshot] = useState<GameStateResponse | undefined>();
+    const [snapshotsLength, setSnapshotsLength] = useState<number>(-1);
     const [startGame, setStartGame] = useState<boolean>(false);
     const [endGame, setEndGame] = useState<boolean>(false);
     const [numStart, setNumStart] = useState<number>(0);
     const [numReset, setNumReset] = useState<number>(0);
     const [winner, setWinner] = useState<string>(WinState.NO_WINNER);
+
+    const snapshots = useRef<Array<GameStateResponse>>([]);
 
     function initPlayerStates(players: Array<string>): Record<string, PlayerState_I> {
         const states: Record<string, PlayerState_I> = {};
@@ -52,7 +56,7 @@ export function GameManager({lobbyId, playerId_self, players}:Props) {
             return "OPPONENT WON!"
         }
     }
-
+    
     useEffect(() => {
         const handleKeyBoardEvent = (e:KeyboardEvent): void => {
             if (KEYBINDS.find((str: string) => str === e.key) && startGame) {
@@ -99,19 +103,42 @@ export function GameManager({lobbyId, playerId_self, players}:Props) {
                 setStartGame(false);
                 setEndGame(false);
                 setNumStart(0);
+
+                snapshots.current = [];
+                setSnapshot(undefined);
+                setSnapshotsLength(-1);
             }
         });
 
         socket.on(Events.UpdateGame, (data) => {
-            setPlayerStates(data.state.players);
-            setPipeState(data.state.pipe);
+            snapshots.current.push(data.state);
+            if (snapshots.current.length >= 4) {
+                const snapshot: GameStateResponse | undefined =snapshots.current.shift();
+                if (snapshot) {
+                    setSnapshot(snapshot);
+                    setPipeState(snapshot.pipe);
+                    setPlayerStates(snapshot.players);
+                }
+            }
         });
 
         socket.on(Events.EndGame, (data) => {
-            setEndGame(true);
-            setWinner(data.winner);
+            const id = setInterval(() => {
+                const snapshot: GameStateResponse | undefined =snapshots.current.shift();
+                if (snapshot) {
+                    setSnapshot(snapshot);
+                    setPipeState(snapshot.pipe)
+                    setPlayerStates(snapshot.players);
+                    setSnapshotsLength(snapshots.current.length);
+                }
+                else {
+                    setEndGame(true);
+                    setSnapshotsLength(-1);
+                    setWinner(data.winner);
+                    clearInterval(id);
+                }
+            }, game_tick)
         });
-
 
         return () => {
             socket.off(Events.StartGame);
@@ -120,6 +147,49 @@ export function GameManager({lobbyId, playerId_self, players}:Props) {
             socket.off(Events.EndGame);
         }
     }, [socket, players, numStart, numReset]);
+
+
+    useEffect(() => {
+        if (snapshotsLength === 0 && startGame) {
+            setEndGame(true);
+            setSnapshotsLength(-1);
+        }
+    }, [snapshotsLength, startGame]);
+
+    useEffect(() => {
+        let raf: number = 0;
+        const PIPE_VELOCITY = 0.5;
+
+        const interpolatePipe = (gapCoords: BoxCoordinates, maxGapCoords: BoxCoordinates, accumulator: number): void => {
+            const start = performance.now();
+            const newGapCoords: BoxCoordinates = calculateNewGapCoords(gapCoords, PIPE_VELOCITY)  
+            const accumulated = performance.now() - start + accumulator;
+            if (accumulated < game_tick  && newGapCoords.topLeft.x > maxGapCoords.topLeft.x) {
+                setPipeState({
+                    gapCoords: newGapCoords
+                });
+                raf = window.requestAnimationFrame(() => interpolatePipe(newGapCoords, maxGapCoords, accumulated));
+            }
+        }
+
+        if (!endGame && snapshot && startGame) {
+            const snapshotGapCoords = snapshot.pipe.gapCoords;
+            const maxGapCoords: BoxCoordinates = calculateNewGapCoords(snapshotGapCoords, GAME_DIMENSIONS.PIPE_VELOCITY);
+    
+            if (snapshotGapCoords.topRight.x > 0) {
+                window.cancelAnimationFrame(raf);
+                interpolatePipe(snapshotGapCoords, maxGapCoords, 0);
+            }
+        }
+        else {
+            window.cancelAnimationFrame(raf);
+        }
+
+        return () => {
+            window.cancelAnimationFrame(raf);
+        }
+
+    }, [snapshot, endGame, startGame]);
 
     return (
         <>
